b95b5d521f642ed0731e371c4602c00ca14922a0
Maor <maorsa9@gmail.com>
2024-01-11 10:52:02 +0200

    delete unnecessary files

--- routes/landlordsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authController = require('./../controllers/authController');
-
-//middleware
-const router = express.Router();
-router.post('/signup', authController.signup);
-
-module.exports = router;
