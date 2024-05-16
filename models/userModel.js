fad41fb7369d323c12d74df47d8302a12798ee56
Maor <maorsa9@gmail.com>
2024-05-16 11:45:05 +0300

    added array to academic model

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const Apartment = require("./apartmentModel");
 
 const userSchema = new mongoose.Schema({
   pushToken: {
