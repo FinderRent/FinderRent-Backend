1da5465d4bac8ca65a7494ee1ee22d6f3874d2c7
Maor <maorsa9@gmail.com>
2024-05-30 20:18:08 +0300

    added getAllChats routes and controller

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router
   .route("/")
-  .post(chatController.createChat)
-  .get(chatController.getAllChats);
+  .get(chatController.getAllChats)
+  .post(chatController.createChat);
+
 router.patch("/update/:chatId", chatController.updateChat);
 router.get("/:userId", chatController.userChats);
 router.get("/find/:firstId/:secondId", chatController.findChat);
